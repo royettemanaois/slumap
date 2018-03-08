16b5be9a22b6e7d8e10d966c8ed9806f9349d76d
culanag <34410165+culanag@users.noreply.github.com>
2018-03-08 16:18:51 +0800

    Removed a useless attribute

--- interactionsv2.js
+++ interactionsv2.js
@@ -355,7 +355,7 @@ function pointOfInterestDetails(num, arrayIndex) {
 
 	}
 	if(obj.image == ""){
-        	content += "<form action='upload.php' method='post' enctype='multipart/form-data'> Select image to upload: <input type='file' name='fileToUpload' id='fileToUpload'> <input type='submit' value='Upload Image' name='submit' onclick='getFileName()'></form>";
+        	content += "<form action='upload.php' method='post' enctype='multipart/form-data'> Select image to upload: <input type='file' name='fileToUpload' id='fileToUpload'> <input type='submit' value='Upload Image' name='submit'></form>";
         
         
     }else{
